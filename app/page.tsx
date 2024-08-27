a9662450b21a353b23630f985d282f218dd3a453
Lucas Breder <lucasbreder@gmail.com>
2024-08-27 18:26:05 -0300

    feat: criado componente de link

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Button from "@/components/Button/Button";
-import Link from "@/components/Link/Link";
+import Link from "@/components/Link/LinkItem";
 import Selector from "@/components/Selector/Selector";
 
 export default function Home() {
