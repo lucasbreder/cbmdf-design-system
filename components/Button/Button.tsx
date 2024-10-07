e7036bc4c729dc8528e9c69860a8c7fe4e699dc4
Lucas Breder <lucasbreder@gmail.com>
2024-10-07 18:39:03 -0300

    feat:Inicia construção da página de catálogo

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { IconName } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core';
